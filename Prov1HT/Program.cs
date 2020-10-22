af2bca6879093f7026e2d2138d9659933942783a
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-10-22 15:11:48 +0200

    Initial Commit

---
+++ Prov1HT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov1HT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
