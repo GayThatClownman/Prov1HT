d5698de847a033fd5634de316ca03b28d8f4c4a6
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-10-22 15:32:31 +0200

    Variabler och metoder för klassen "Book" skapade.

---
+++ Prov1HT/Book.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Prov1HT
+{
+    public class Book
+    {
+        public int price;
+        private string name;
+        private int rarity;
+        private string category;
+        private int actualValue;
+        private bool cursed;
+        private Random generator;
+
+        public Book()
+        {
+
+        }   
+
+        public void PrintInfo()
+        {
+
+        }
+
+        // public int Evaluate()
+        // {
+            
+        // }
+
+        // public string GetCategory()
+        // {
+
+        // }
+
+        // public string GetName()
+        // {
+
+        // }
+
+        // public bool IsCursed()
+        // {
+
+        // }
+    }
+}
